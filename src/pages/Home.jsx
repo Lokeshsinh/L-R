26370277af8cf116d5014c1cddcd0a1976f2e391
LokeshSinh <lokeshsingh.jk@gmail.com>
2026-07-13 21:57:06 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/common/Header";
 import Footer from "../components/common/Footer";
 
-import Hero from "../components/home/Hero";
+import Hero from "../components/home/Hero.jsx";
 import ClientLogoStrip from "../components/home/ClientLogoStrip";
 import ProductCategory from "../components/home/ProductCategory";
 import AboutPreview from "../components/home/AboutPreview";
